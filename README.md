705113c931851150d68ec8126ee48d90cd6c96ae
Vitor Gomes <vconrado@gmail.com>
2017-09-15 08:35:57 -0300

    updating README example with sort_by option

--- README.md
+++ README.md
@@ -30,8 +30,9 @@ fc_all = b.feature_collection("esensing:focos_bra_2016")
 
 # Retrieving selected 'focos' of esensing:focos_bra_2016
 fc = b.feature_collection("esensing:focos_bra_2016", 
-                          attributes=("id","municipio","timestamp"), 
+                          attributes=("id","municipio","estado","timestamp"),
                           within="POLYGON((-49.8929205749999 1.21448026000007,-50.945202767 0.671596891000149,-51.2334818149999 0.0142109470000378,-49.8929205749999 1.21448026000007))", 
-                          filter=["vegetacao='2'","(satelite='NPP_375'+OR+satelite='TERRA_M-T')"], 
+                          filter=["vegetacao='2'","(satelite='NPP_375'+OR+satelite='TERRA_M-T')"],
+                          sort_by=("estado","municipio"),
                           max_features=10)
 ```
--- examples/test.py
+++ examples/test.py
@@ -5,7 +5,7 @@ sys.path.insert(0, '../src/bdq')
 
 from bdq import bdq
 
-b = bdq("http://my_geoserver:8080/geoserver-esensing/", debug=True)
+b = bdq("http://my_geoserver:8080/geoserver-esensing/", debug=False)
 
 ft_list = b.list_features()
 
@@ -19,7 +19,7 @@ fc = b.feature_collection("esensing:focos_bra_2016",
                           attributes=("id", "municipio", "timestamp", "regiao"),
                           within="POLYGON((-49.515628859948507 -19.394602563415745,-48.020567850467053 -19.610579617637825,-48.354439522883652 -21.052347219666608,-49.849500507163917 -20.836369963642884,-49.515628859948507 -19.394602563415745))",
                           filter=["satelite_referencia='true'", "timestamp>='2016-01-01'", "timestamp<'2016-02-01'"],
-                          sort_by="regiao,municipio",
+                          sort_by=("regiao", "municipio"),
                           max_features=10)
 # print(fc)
 for f in fc['features']:
