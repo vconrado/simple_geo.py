1601e884f7d20620b693812fd2faba20ef0cdb63
Vitor Gomes <vconrado@gmail.com>
2017-10-07 23:26:57 -0300

    cleaning example and test folders

--- examples/example1.py
+++ examples/example1.py
@@ -1,60 +1,50 @@
+# -*- coding: utf-8 -*-
 import sys
 
 # to use local version of simple_geo
 sys.path.insert(0, '../src/simple_geo')
 
 from simple_geo import simple_geo as sgeo
+from osgeo import ogr
 
 ######################################################################################################################
 # simple_geo Features
 
-s = sgeo(wfs="http://localhost:8080/geoserver-esensing", wtss="http://localhost:7654/ts", debug=True)
+# fazer cache
+s = sgeo(wfs="http://localhost:8080/geoserver-esensing/", wtss="http://localhost:7644", debug=False)
 
 print("Features")
+
 # Retrieving the list of all available features in the service
-ft_list = s.list_features()
-print(ft_list)
+# ft_list = s.list_features()
+# print(ft_list)
 
-# Retrieving the metadata of a given feature
-ft_scheme = s.describe_feature("esensing:focos_bra_2016")
-print(ft_scheme)
+ft_desc = s.describe_feature('esensing:municipios_bra')
+print("description: ", ft_desc)
 
-# Retrieving the collection for a given feature
-#fc, fc_metadata = b.feature_collection("esensing:focos_bra_2016")
+ft_desc = s.describe_feature('esensing:focos_bra_2016')
+print("description: ", ft_desc)
 
+fc, fc_metadata = s.feature_collection("esensing:estados_bra",
+                                       filter={"nome": 'Santa Catarina'}, max_features=1)
 
-# Retrieving a selected elements for a given feature
-fc, fc_metadata = s.feature_collection("esensing:focos_bra_2016",
-                                       attributes=("id", "municipio", "timestamp", "regiao"),
-                                       within="POLYGON((-49.515628859948507 -19.394602563415745,-48.020567850467053 -19.610579617637825,-48.354439522883652 -21.052347219666608,-49.849500507163917 -20.836369963642884,-49.515628859948507 -19.394602563415745))",
-                                       filter=["satelite_referencia='true'", "timestamp>='2016-01-01'",
-                                               "timestamp<'2016-02-01'"],
-                                       sort_by=("regiao", "municipio"),
-                                       max_features=10)
+print("metadata: ", fc_metadata)
 print(fc)
-print(fc_metadata)
 
-# Retrieving collection length of selected elements for a given feature
-fc_len = s.feature_collection_len("esensing:focos_bra_2016",
-                                  within="POLYGON((-49.515628859948507 -19.394602563415745,-48.020567850467053 -19.610579617637825,-48.354439522883652 -21.052347219666608,-49.849500507163917 -20.836369963642884,-49.515628859948507 -19.394602563415745))",
-                                  filter=["satelite_referencia='true'", "timestamp>='2016-01-01'",
-                                          "timestamp<'2016-02-01'"])
-print(fc_len)
+fc2, fc2_metadata = s.feature_collection("esensing:municipios_bra",
+                                         spatial_filter={'within': fc.loc[0, 'geometry'].wkt,
+                                                         'intersects': fc.loc[0, 'geometry'].wkt},
+                                         max_features=2,
+                                         attributes=['nome', 'geom', 'estado'],
+                                         sort_by={"nome": "DESC"},  # 'nome', ['nome', 'estado']
+                                         filter={"regiao": "S"})
 
-######################################################################################################################
-# BDQ Coverages
+print("metadata: ", fc2_metadata)
+print(fc2)
 
-print("\n\nCoverages")
+fc_len = s.feature_collection_len("esensing:municipios_bra",
+                                  spatial_filter={'within': fc.loc[0, 'geometry'].wkt,
+                                                  'intersects': fc.loc[0, 'geometry'].wkt},
+                                  filter={"regiao": "S"})
 
-# Retrieving the list of all available coverages in the service
-cv_list = s.list_coverages()
-print(cv_list)
-
-# Retrieving the metadata of a given coverage
-cv_scheme = s.describe_coverage("climatologia")
-print(cv_scheme)
-
-# Retrieving the time series for a given location
-ts, ts_metadata = s.time_series("climatologia", ("precipitation", "temperature", "humidity"), -12, -54)
-print(ts)
-print(ts_metadata)
+print('fc_len', fc_len)
